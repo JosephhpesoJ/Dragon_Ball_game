1877d6e3ab407f3062236ef00b43b17cc08ec76e
JosephhpesoJ <Jchoi100609@gmail.com>
2025-12-17 15:35:11 -0600

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Goku":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . f f f . . . 
+. . . f f . . . f f f f . . . . 
+. . . . f f . f f f f . . . . . 
+. . . . . f f f f f f f f f . . 
+. . . . . f f f f f f f f . . . 
+. . . f f f f f f f f f . . . . 
+. . . . . f d d d d f . . . . . 
+. . . . . d f d d f d . . . . . 
+. . . . . d d d d d d . . . . . 
+. . . . 4 4 4 8 8 4 4 4 . . . . 
+. . . . 8 4 4 4 4 4 4 8 . . . . 
+. . . . d 4 4 4 4 4 4 d . . . . 
+. . . . d 8 8 8 8 8 8 d . . . . 
+. . . . . 4 4 . . 4 4 . . . . . 
+. . . . . 8 8 . . 8 8 . . . . . 
+`;
+            case "image2":
+            case "Vegeta":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . f . . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f f f f f . . . . . 
+. . . . . d d f f d d . . . . . 
+. . . . . d f d d 7 7 . . . . . 
+. . . . . d d d d d d . . . . . 
+. . . 5 5 8 8 8 8 8 8 5 5 . . . 
+. . . . 8 8 8 8 8 8 8 8 . . . . 
+. . . . 8 8 8 8 8 8 8 8 . . . . 
+. . . . 1 e e e e e e 1 . . . . 
+. . . . . 8 8 . . 8 8 . . . . . 
+. . . . . 1 1 . . 1 1 . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
